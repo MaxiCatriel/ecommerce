23ba13ef43e3dde6b7b9da4d59172fc9ce66cde2
mcatrielkaizen <catriel_insegna@hotmail.com>
2026-03-11 21:19:36 -0300

    feat(home): autoplay carousel with seamless CSS animation

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -19,7 +19,7 @@ export async function Carousel({ showPrices }: { showPrices: boolean }) {
   const carouselProducts = [...products, ...products, ...products];
 
   return (
-    <div className="w-full overflow-x-auto pb-6 pt-1">
+    <div className="w-full overflow-hidden pb-6 pt-1">
       <ul className="flex animate-carousel gap-4">
         {carouselProducts.map((product, i) => (
           <li
